738f5d42e8be8d22e9fcd465e340730d60c397bc
Yuwarajalingam Dinesh <yuwarajalingamd@gmail.com>
2024-06-17 14:01:57 -0400

    updated get reciept api with user validation

--- src/objects/reciepts/controllers/getReciept.ts
+++ src/objects/reciepts/controllers/getReciept.ts
@@ -14,12 +14,13 @@ const getReciept = async (req: AuthenticatedRequest, res: Response, next: NextFu
         const receiptId = req.params.id;
         const receipt = await Receipt.findById(receiptId); 
 
+
         if (receipt?.created_by) {
-            if (receipt.created_by !== new Types.ObjectId(memberId)) {
+            if (receipt.created_by.toString() !== memberId) {
                 return res.status(400).send("Non authenticated reciept call");
             }
         }
-        
+
         if (!receipt) return res.status(404).send('Receipt not found.');
     
         return res.status(201).send(receipt);
